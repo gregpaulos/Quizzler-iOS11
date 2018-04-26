70a6c19c74d5e16b997d1bdc8da5cde4d1f8de8d
gregory paulos <gregorypaulos@gregorys-MacBook-Air.local>
2018-04-26 17:00:35 -0600

    basic functionality

---
+++ Quizzler/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by gregory paulos on 4/26/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    
+    let questionText : String
+    let answer : Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+    
+}
